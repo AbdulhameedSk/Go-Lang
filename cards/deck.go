4ac5bba3ffd0c7db382c6419abe89169a688245b
abdulhameedskk <shaikabdulhameedd@gmail.com>
2025-06-03 12:25:05 +0530

    newDeck

--- cards/deck.go
+++ cards/deck.go
@@ -6,8 +6,29 @@ import "fmt"
 //which is a slice of strings
 type deck []string //same as type deck extends []string
 
-// Reciever sets up methods on variable we create
+type card struct {
+	// Define a struct to represent a card
+	suit string
+	rank string
+}
 
+//newDeck function creates and returns a new deck of cards essentially a Array of strings
+func newDeck() deck {
+	// Create a new deck of cards with some initial values
+	cardSuits := []string{"Hearts", "Diamonds", "Clubs", "Spades"}
+	cardValues := []string{"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"}
+	// Initialize an empty deck
+	cards := deck{}
+	for _, s := range cardSuits {
+		for _, v := range cardValues {
+			cards = append(cards, v+" of "+s) // Append each card to the deck})
+		}
+	}
+	// Return the newly created deck
+	return cards
+}
+
+// Reciever sets up methods on variable we create
 //The actual copy of cards is avalilable in deck as d in the function
 //Every variable of type deck will have access to the print method
 func (d deck) print() {
--- cards/main.go
+++ cards/main.go
@@ -5,12 +5,14 @@ package main
 func main() {
 	//Static type Language variables declared using data types
 	// cards:=[]string{"Ace of Spades", "Two of Hearts", "Three of Clubs", newCard()}
-	cards := deck{"Ace of Spades", "Two of Hearts", "Three of Clubs", newCard()}
-	cards = append(cards, "Six of Spades") // Adding a new card to the slice
-	cards.print()                          // now d is cards
+	// cards := deck{"Ace of Spades", "Two of Hearts", "Three of Clubs", newCard()}
+	// cards = append(cards, "Six of Spades") // Adding a new card to the slice
+	cards := newDeck() // Create a new deck of cards
+	cards.print()      // now d is cards
 
 }
-func newCard() string {
-	// var card string = "Ace of Spades"
-	return "Five of Diamonds" // Using short variable declaration
-}
+
+// func newCard() string {
+// 	// var card string = "Ace of Spades"
+// 	return "Five of Diamonds" // Using short variable declaration
+// }

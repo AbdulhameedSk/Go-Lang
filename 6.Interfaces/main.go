8cb24f33f88799b7e906de94e7dd3b53a31ee3b8
abdulhameedskk <shaikabdulhameedd@gmail.com>
2025-06-08 00:35:02 +0530

    Interfaces

---
+++ 6.Interfaces/main.go
@@ -0,0 +1,42 @@
+//Interfaces in Go helps to define a contract that a type must adhere to.
+package main
+import "fmt"
+//we have a type called bot which is interface.
+//if you are type in this program with function called greet, and return string,you are member of type bot.
+//since both EnglishBot and SpanishBot implement the greet method, they are considered to be of type bot.
+// The bot interface defines a method for greeting. 
+type bot interface{
+	greet() string
+}
+type EnglishBot struct{}
+type SpanishBot struct{}
+
+func main(){
+	eb:= EnglishBot{}
+	sb := SpanishBot{}
+	printGreeting(eb) // Output: Hello
+	printGreeting(sb) // Output: Hola
+
+}
+
+func (EnglishBot) greet() string {
+	return "Hello"
+}
+func (SpanishBot) greet() string {
+	return "Hola"
+}
+// Greet is an interface that defines a method for greeting.
+
+// func printGreeting(e EnglishBot) {
+// 	// The function accepts any type that implements the Greet interface.
+// 	fmt.Println(e.greet())
+// }
+
+// func printGreeting(s SpanishBot){
+// 	fmt.Println(s.greet())
+// }
+
+func printGreeting(b bot) {
+	// The function accepts any type that implements the bot interface.
+	fmt.Println(b.greet())
+}

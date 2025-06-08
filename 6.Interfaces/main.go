f0dfa647a3ab3f73224f1771c36b0a33920954cf
abdulhameedskk <shaikabdulhameedd@gmail.com>
2025-06-08 11:15:30 +0530

    Small commit

--- 6.Interfaces/main.go
+++ 6.Interfaces/main.go
@@ -1,12 +1,18 @@
-//Interfaces in Go helps to define a contract that a type must adhere to.
+// Interfaces in Go helps to define a contract that a type must adhere to.
+//Interfaces are generic, meaning they can be used with any type that implements the methods defined in the interface.
+//Interface are implicit, which means that you don't have to explicitly declare that a type implements an interface.
+
 package main
+
 import "fmt"
+
 //we have a type called bot which is interface.
 //if you are type in this program with function called greet, and return string,you are member of type bot.
 //since both EnglishBot and SpanishBot implement the greet method, they are considered to be of type bot.
-// The bot interface defines a method for greeting. 
+// The bot interface defines a method for greeting.
 type bot interface{
 	greet() string
+	// greet() (string) (string, error)
 }
 type EnglishBot struct{}
 type SpanishBot struct{}

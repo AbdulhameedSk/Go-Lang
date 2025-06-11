2ffed33085e931362c309becfdd8ffa24370344a
abdulhameedskk <shaikabdulhameedd@gmail.com>
2025-06-11 22:13:39 +0530

    More about responses and Interfaces

--- 7.Interfaces_HTTP/main.go
+++ 7.Interfaces_HTTP/main.go
@@ -5,7 +5,10 @@ import (
 	"net/http"
 	"os"
 )
-
+//Response is a struct type response has (status code-String, body-io.ReadCloser Interface, StatusCode-int)
+//when it comes to body it is interface which further has interfaces named as Reader and Closer
+// io.Reader is an interface that wraps the Read method, which reads data from a source.
+// io.Closer is an interface that wraps the Close method, which closes the resource.
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {

6f437bf177d4b9f26f4d423ea0cd8c313ddcfaff
abdulhameedskk <shaikabdulhameedd@gmail.com>
2025-06-17 14:41:32 +0530

    Assignment provided

---
+++ 12.Hard_Assignment/main.go
@@ -0,0 +1,7 @@
+//Create a program that reads contents of a text file then prints its content to the terminal
+//The file name is provided as argument in terminal EG: go run main.go myfile.txt
+//Refer os.Args
+//To open a file use os.Open
+
+//What interface does file type implement?
+//If the 'File' type implements the 'io.Reader' interface, then you can use the 'io.ReadAll' function to read the file contents.

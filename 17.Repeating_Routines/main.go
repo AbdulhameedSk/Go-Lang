5020aa0518ce804f50933f349306f9aaf0907c31
abdulhameedskk <shaikabdulhameedd@gmail.com>
2025-06-20 00:55:07 +0530

    Gotcha

---
+++ 17.Repeating_Routines/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"https://www.google.com",
+		"https://www.example.com",
+		"https://www.test.com",
+		"https://www.github.com",
+		"https://www.stackoverflow.com",
+		"https://www.reddit.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	// for  {
+	// 	go checkLink(<-c,c)
+	// }
+	for l := range c {
+		// go checkLink(l, c)
+		go func() {
+			time.Sleep(5 * time.Second)
+			checkLink(l, c)
+		}()
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "is down")
+		c <- link
+		return
+	}
+	fmt.Println(link, "is up")
+	c <- link
+}
